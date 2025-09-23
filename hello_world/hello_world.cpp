a6804128d29b7dff3ef8f311c6b831fc540e29d1
Evan Drake <31263887+snowmang1@users.noreply.github.com>
2025-09-23 20:38:47 +0000

    ;lakjsd;fljasdf;lkjasdl;kfjl;aksdf

--- hello_world/hello_world.cpp
+++ hello_world/hello_world.cpp
@@ -1,7 +1,23 @@
 // cpp code
-#include <cstdio>
+#include <cstdio> // includes printf library
+#include <array>  // includes array library
+#include <string> // to use strings
+
+// \n endline character
 
 int main() {
-    printf("hello world");
+
+    std::array<char, 8> squid = {'h','i', ' ', 't', 'h', 'e', 'r', 'e'};
+
+    std::printf("%c\n", squid.at(1));
+
+    std::array<std::string, 2> squid_str = {"hi", "there"};
+
+    std::printf("%s\n", squid_str.at(0).c_str());
+
+
+    char ex = 'B';
+
+    std::printf("this is a number %i\n", ex);
     return 0;
 }
